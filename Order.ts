5553907cded70b8cf3df66a01cecbf6ce641dd3d
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-26 16:47:29 -0600

    Se actualiza Orden
    En esta actualizacion se agregan getters y tambien se agrega el metodo toString, donde mostrara la orden pero como cadena

--- Order.ts
+++ Order.ts
@@ -1,12 +1,33 @@
 
+//Se utilizara el nombre de la compañia, la cantidad de acciones y el precio individual
+//que se ofrece por ellas 
 export class Order {
-    private company: string 
-    private cuantity: number
-    private purchase_price: number
+    private company: string;
+    private quantity: number;
+    private purchase_price: number;
 
-    constructor(company: string, cuantity: number, purchase_price: number) {
-        this.company = company
-        this.cuantity = cuantity
-        this.purchase_price = purchase_price
+    constructor(company: string, quantity: number, purchase_price: number) {
+        this.company = company;
+        this.quantity = quantity;
+        this.purchase_price = purchase_price;
+    }
+
+    //Aignacion de getters
+    public getCompany(): string {
+        return this.company;
+    }
+
+    public getQuantity(): number {
+        return this.quantity;
+    }
+
+    //Con este get yo puedo comparar los precios y dependiendo del valor, ordenar las acciones
+    public getPurchasePrice(): number {
+        return this.purchase_price;
+    }
+
+    //Este metodo me sirve para representar la orden como cadena
+    public toString(): string {
+        return `${this.company}=> ${this.quantity} acciones a $${this.purchase_price} c/u`;
     }
 }

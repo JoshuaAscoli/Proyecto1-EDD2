9eb7a202fe12a78304d5bb7a9005a3ffda2bd181
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-29 16:56:06 -0600

    Finalización del proyecto
    En este ultimo cambio se finaliza el proyecto agregando una función que agrega un archivo txt donde se verán todas las transacciones realizadas

--- transactions.ts
+++ transactions.ts
@@ -1,25 +1,34 @@
+//se importa purchase y sale Arrays para utilizar las ordenes y acceder a los atributos
 import { purchaseArray, saleArray } from "./Orders";
+// se importa fs para crear el history_details.txt
+import * as fs from 'fs';
+const fileName = 'History_details.txt';
 
-
-// Aquí puedes realizar tus comparaciones de precios
-console.log('COINCIDENCIAS ENTRE COMPRAS Y VENTAS:');
+//se hace un for que recorra las compras y compare con otro for que recorra las ventas
+//asi poder realizar las comprobaciones
 for (let purchase of purchaseArray) {
     for (let sale of saleArray) {
         if (purchase.getCompany() == sale.getCompany()) {
             if (purchase.getPurchasePrice() >= sale.getSalePrice() && sale.getQuantity() >= purchase.getQuantity()) {
-                console.log(`Coincidencia encontrada: 
+                const transaccion = `----------Transacción realizada----------:
 Compañía: ${purchase.getCompany()}
-Cantidad ofrecida: ${purchase.getPurchasePrice()} 
+Cantidad ofrecida: ${purchase.getPurchasePrice()}
 Precio de venta: ${sale.getSalePrice()}
 Acciones de compra: ${purchase.getQuantity()}
 Acciones de venta: ${sale.getQuantity()}
-Acciones restantes de ${sale.getCompany()}:`, sale.getQuantity() - purchase.getQuantity() );
+Acciones restantes de ${sale.getCompany()}: ${sale.getQuantity() - purchase.getQuantity()}\n`;
 
+//Si no existiera, se crea el archivo y se agrega las demas coincidencias
+fs.appendFile(fileName, transaccion, (err) => {
+    if (err) {
+        console.error('Error al crear el archivo:', err);
+    } else {
+        console.log(`Archivo ${fileName} creado con éxito.`);
+    }});
 
-console.log('-------------------------------------------------')
-            } else {
-                console.log(`La compañía ${purchase.getCompany()} tiene más compras (${purchase.getQuantity()}) que ventas (${sale.getQuantity()}).`);
             }
         }
     }
 }
+
+
